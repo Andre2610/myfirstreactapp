e1d14776528054e2a8303bddb0fb1ed52c59c9e0
Andre2610 <dre2610@gmail.com>
2020-05-18 15:17:19 +0200

    content and style added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Codaisseur
-        </a>
-      </header>
-    </div>
+    <main className="my-5">
+      <div className="mb-4">
+        <Title content="Some simple text" />
+      </div>
+      <div className="row">
+        {all_pokemon.map(function (pokemon) {
+          return (
+            <div className="col-md-6 cold-lg-4">
+              <Pokemon
+                name={pokemon.name}
+                weight={pokemon.weight}
+                awesome={pokemon.awesome}
+                terrifying={pokemon.terrifying}
+                abilities={pokemon.abilities}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </main>
   );
 }
 
